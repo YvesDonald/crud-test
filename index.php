52874477bb70a61620ba3d22ca0b0128b21fabd2
Edithson <moafogaus@gmail.com>
2024-06-17 05:07:08 +0100

    Mise à jour du fichier index.php

--- index.php
+++ index.php
@@ -2,4 +2,16 @@
 echo "Hello";
 
 echo "Excellent";
+
+$myName = "All Out";
+
+function surcharge($name=null){
+    if (!is_null($name)) {
+        echo "Bonjour"+$name;
+    }else{
+        echo "Bonjour";
+    }
+}
+
+surcharge($myName);
 ?>
